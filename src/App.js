21cb55dad61174a91cd39bb3033368f06f276f13
yashpurkar <yashpurkar7079@gmail.com>
2022-11-23 18:39:24 +0530

    done with UseReducer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import UseState from "./Hooks/UseState";
-import UseEffect from "./Hooks/UseEffect";
+// import UseEffect from "./Hooks/UseEffect";
+import UseReducer from "./Hooks/UseReducer";
 
 function App() {
   return (
     <>
       {/* <UseState /> */}
-      <UseEffect />
+      {/* <UseEffect /> */}
+      <UseReducer />
     </>
   );
 }
---
+++ src/Hooks/UseReducer.js
@@ -0,0 +1,33 @@
+import React, { useReducer } from 'react'
+import "./CSS/index.css"
+
+const reducer = (state, action) => {
+  if (action.type === "INCR") {
+    state = state + 1;
+  }
+  else if (action.type === "DECR" && state > 0) {
+    state = state - 1
+  }
+  return state;
+}
+const UseReducer = () => {
+  const initialData = 0;
+  const [state, dispatch] = useReducer(reducer, initialData)
+
+
+  return (
+    <>
+      <div className="main_container">
+        <div className='container'>
+          <p className="data">
+            {state}
+          </p>
+          <button onClick={() => dispatch({ type: "INCR" })}>+</button>
+          <button onClick={() => dispatch({ type: "DECR" })}>-</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default UseReducer;
